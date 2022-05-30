d589b8ce831c2dae4483abb11f3003406672ef5d
Matthew <105758744+matthew-james-brewer@users.noreply.github.com>
2022-05-30 09:07:30 -0500

    Update mail.js

--- mail.js
+++ mail.js
@@ -1,3 +1,3 @@
 function submit() {
-let mailto = 'mailto:'+document.getElementById('recipients')+','+document.getElementById('phones')+'?bcc='+document.getElementById('bcc')+document.getElementById('phone_bcc')+'&subject='+document.getElementById('subject')+'&body='+document.getElementById('body');
+let mailto = 'mailto:'+document.getElementById('recipients')+'?bcc='+document.getElementById('bcc')+'&subject='+document.getElementById('subject')+'&body='+document.getElementById('body');
 }
