d431dc953ae08a6d50b77fc6fe5be5991d08c2b5
Matthew <105758744+matthew-james-brewer@users.noreply.github.com>
2022-07-05 12:34:39 -0500

    Update mail.js

--- mail.js
+++ mail.js
@@ -3,3 +3,16 @@ function submit() {
 let mailto = 'mailto:'+form.elements[0].value+'?bcc='+form.elements[1].value+'&subject='+form.elements[2].value+'&body='+form.elements[3].value;
 	window.open(mailto,'_blank','height='+screen.height+',width='+screen.width);
 }
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+} 
+			function submitForm(event){
+        event.preventDefault();
+        submit();
+    }
